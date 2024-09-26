8bee88958c543c56a7c670cecac18c81a872c5b1
Cathyprime <magdajaroszynska47@gmail.com>
2024-09-26 21:27:38 +0200

    feat: display ui alongside playing sounds

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 mod musak_player;
+mod gui;
 
 use musak_player::{musak, MusakError};
+use gui::run_ui as start_ui;
 use std::thread;
+use std::sync::mpsc::channel;
 
 fn main() -> Result<(), MusakError> {
-    let mut result = Result::Ok(());
-    thread::scope(|s| {
-        s.spawn(|| result = musak());
-    });
-    result
+    let (tx, rx) = channel();
+
+    let handle = thread::spawn(move || musak(tx));
+    start_ui();
+    handle.join().unwrap();
+
+    rx.recv().unwrap()
 }
--- src/musak_player.rs
+++ src/musak_player.rs
@@ -1,5 +1,6 @@
 use rodio::{Decoder, OutputStream, Sink, Source};
 use std::{fs::File, io::BufReader};
+use std::sync::mpsc::Sender;
 
 #[derive(Debug)]
 pub(crate) enum MusakError {
@@ -38,7 +39,11 @@ impl std::fmt::Display for MusakError {
 	}
 }
 
-pub(crate) fn musak() -> Result<(), MusakError> {
+pub(crate) fn musak(tx: Sender<Result<(), MusakError>>) {
+    tx.send(m_musak()).unwrap();
+}
+
+fn m_musak() -> Result<(), MusakError> {
 	let (_stream, stream_handle) = OutputStream::try_default()?;
 	let sink = Sink::try_new(&stream_handle)?;
 	let sink2 = Sink::try_new(&stream_handle)?;
@@ -47,16 +52,12 @@ pub(crate) fn musak() -> Result<(), MusakError> {
 		let stream = BufReader::new(File::open("assets/stream.ogg")?);
 		Decoder::new_looped(stream)?
 			.repeat_infinite()
-			.take_duration(std::time::Duration::from_secs(1))
-			.amplify(1.)
 	};
 
 	let birds_source = {
 		let birds = BufReader::new(File::open("assets/birds.ogg")?);
 		Decoder::new_looped(birds)?
 			.repeat_infinite()
-			.take_duration(std::time::Duration::from_secs(1))
-			.amplify(1.)
 	};
 
 	sink.set_volume(0.2);
